e1507c8c7bcd70a67d01460070bc88e977731d9b
HakkaOfDev <frestytv@gmail.com>
2022-05-09 16:43:00 +0200

    [FEATURE] Language system

--- src/types/language.ts
+++ src/types/language.ts
@@ -1,8 +1,20 @@
 import { ResourceLanguage } from 'i18next';
 
+export enum Regions {
+  EU='Europe',
+  NA='North America',
+  AS='Asia',
+  OC='Oceania',
+  AF='Africa',
+  SA='South America',
+  AN='Antarctica',
+  UNKNOWN='Unknown'
+}
+
 export type Language = {
   code: string;
   translationKey: string;
   flagCode: string;
   translationFile: ResourceLanguage;
+  region: string;
 };
